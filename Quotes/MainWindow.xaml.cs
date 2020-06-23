6cb7625af41ab6c65f17d0be3ec674c5591f4e74
BuildTools <unconfigured@null.spigotmc.org>
2020-06-23 14:02:32 -0700

    Add project files.

---
+++ Quotes/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MyQuotes quotes = new MyQuotes();
+        private DateTime today = DateTime.Now;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBlockQuote.Text = quotes.getQuote;
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            textBlockQuote.Text = quotes.getQuote;
+        }
+
+
+    }
+}
---
+++ Quotes/MyQuotes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quotes
+{
+    internal class MyQuotes
+    {
+        private List<string> quoteList = new List<string>();
+
+        public string getQuote
+        {
+            get
+            {
+                Random random = new Random();
+                int rnd = random.Next(0, quoteList.Count());
+                return quoteList[rnd];
+            }
+        }
+
+        public MyQuotes()
+        {
+            loadQuotes();
+        }
+
+        private void loadQuotes()
+        {
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"Quotes.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                quoteList.Add(line);
+            }
+
+            file.Close();
+        }
+    }
+}
